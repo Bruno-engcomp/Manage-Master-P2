1593c5ef75bd776049e2ad1ea2c09d2b4b1c83ae
bruno.almd <brunoalmeida22007@gmail.com>
2026-01-25 12:32:12 -0300

    UserController criado e finalizado

---
+++ sigfebackend/src/main/java/com/sigfe/backend/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.sigfe.backend.controller;
+
+import com.sigfe.backend.dto.user.*;
+import com.sigfe.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    // POST /users
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponseDTO create(@RequestBody UserCreateDTO dto) {
+        return userService.create(dto);
+    }
+
+    // GET /users
+    @GetMapping
+    public List<UserResponseDTO> findAll() {
+        return userService.findAll();
+    }
+}
